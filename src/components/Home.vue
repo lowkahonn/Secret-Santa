96b7772aa3f9e32c1b89b0b99991633b78fea9f0
lowkahonn <onn0106@hotmail.com>
2020-12-12 11:33:57 +0800

    Revert "Align items to center and add a Register view"
    This reverts commit facd9602f1e904be2fb37bb7a3988924b3619c80.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="home">
-      <p class="welcome-msg">Welcome to Secret Santa</p>
-      <div class="santa">
-        <img src="../assets/secret-santa.png">
-      </div>
-      <router-link :to="{ name: 'Registration' }" tag="button" class="register"> Register </router-link>
+    <img src="../assets/secret-santa.png">
   </div>
 </template>
 
@@ -22,7 +18,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .home {
-  position: fixed;
   width: 100%;
   height: 100%;
   background: url("../assets/snowing.gif") no-repeat center center fixed;
@@ -30,43 +25,15 @@ export default {
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
-  align-items: center;
-}
-
-.welcome-msg {
-  align-items: center;
-  font-size: 2rem;
-  padding: 1rem;
-  margin: 0px;
-  margin-top: 2rem;
-}
-
-.santa {
-  align-items: center;
-  width: 100%;
 }
 
 img {
-  padding: 1rem;
-  max-width: 30%;
-  max-height: 30%;
-}
-
-.register {
-  padding: 0.5rem 2rem;
-  font-size: 1rem;
-  align-items: center;
-  border-radius: 1rem;
-}
-
-@media screen and (max-width: 601px) {
-  .welcome-msg {
-    font-size: 5vw;
-  }
-
-  img {
-    max-width: 50%;
-    max-height: 50%;
-  }
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /* text-align: center; */
+  max-width: 100%;
+  max-height: 100%;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -8,12 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: () => import('@/components/Home')
-    },
-    {
-      path: '/register',
-      name: 'Registration',
-      component: () => import('@/components/Registration')
+      component: Home
     }
   ]
 })
