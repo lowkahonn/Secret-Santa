cea61ed8992299f3138642c1eb9b6761819fb4d1
lowkahonn <onn0106@hotmail.com>
2020-12-17 19:28:56 +0800

    Add styles to email html

--- express/services/email/mailer.service.js
+++ express/services/email/mailer.service.js
@@ -4,12 +4,18 @@ require('dotenv').config()
 
 function getHtml(santa, user, roomInfo) {
     let html = `
-    <html>
-    <body>
-    <img src="cid:emailheader"/>
-    <h1>You will be the Secret Santa 🎅🏻 of ...</h1>
-    <h3>🎄 Merry Christmas ${santa.username}!</h3>
-    <p>Exciting time! Starting from now to the Christmas Day, you will be the Secret Santa of <strong>${user.username}</strong>! You will have a budget of ${roomInfo.budget} to buy a secret gift for ${user.username}. `
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </head>
+        <body>
+        <div bgcolor="#ffffff" style="font-size: 16px; font-family: Arial, sans-serif; line-height: 24px; color: '#000000';">
+            <img src="cid:emailheader"/>
+            <h1>You will be the Secret Santa 🎅🏻 of ...</h1>
+            <h3>🎄 Merry Christmas ${santa.username}!</h3>
+            <p>Exciting time! Starting from now to the Christmas Day, you will be the Secret Santa of <strong>${user.username}</strong>! You will have a budget of ${roomInfo.budget} to buy a secret gift for ${user.username}. `
     if (user.wish) {
         html += `
             To make your life easier (or harder), we leaked ${user.username}'s wish that you could refer to 😛</p>
@@ -19,15 +25,17 @@ function getHtml(santa, user, roomInfo) {
         html += '</p>'
     }
     html += `
-        <p>Enjoy the game! :)</p>
-        <br>
-        <div>Cheers,</div>
-        <div>Secret Agents</div>
+            <p>Enjoy the game! :)</p>
+            <br>
+            <div>Cheers,</div>
+            <div>Secret Agents</div>
+        </div>
         </body>
         </html>
     `
     return html
 }
+
 class Mailer {
     static async sendMail(users, roomInfo) {
         let transporter = nodemailer.createTransport({
